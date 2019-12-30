ed3929009810670c2773ac307758f926ae8cc6ed
potionk <48196324+potionk@users.noreply.github.com>
2019-12-30 16:36:06 +0900

    add 1978 solution

---
+++ src/bj1978/Main.java
@@ -0,0 +1,27 @@
+package bj1978;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int numbers = Integer.parseInt(br.readLine());
+        int count = 0;
+        String[] read = br.readLine().split(" ");
+        for (int j = 0; j < read.length; j++) {
+            boolean isPrimeNum = false;
+            int readData = Integer.parseInt(read[j]);
+            for (int k = 2; k < readData; k++) {
+                if (readData % k == 0) {
+                    isPrimeNum = true;
+                    break;
+                }
+            }
+            if (readData!=1&&!isPrimeNum)
+                count++;
+        }
+        System.out.println(count);
+    }
+}
