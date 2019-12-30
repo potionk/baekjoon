cdb578f5bda9043f893aa870727719cf3a460524
potionk <48196324+potionk@users.noreply.github.com>
2019-12-30 13:56:07 +0900

    add 10569 solution

---
+++ src/bj10569/Main.java
@@ -0,0 +1,18 @@
+package bj10569;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int count=Integer.parseInt(br.readLine());
+        for(int i=0; i<count; i++){
+            String[] read=br.readLine().split(" ");
+            int vertex=Integer.parseInt(read[0]);
+            int edge=Integer.parseInt(read[1]);
+            System.out.println(2+edge-vertex);
+        }
+    }
+}
