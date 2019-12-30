2d8dae7acad0c1b267b3f4829cc38197ef2415f8
potionk <48196324+potionk@users.noreply.github.com>
2019-12-30 17:52:56 +0900

    1978 solution code refactoring

--- src/bj1978/Main.java
+++ src/bj1978/Main.java
@@ -7,19 +7,19 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int numbers = Integer.parseInt(br.readLine());
+        br.readLine();
         int count = 0;
         String[] read = br.readLine().split(" ");
         for (int j = 0; j < read.length; j++) {
-            boolean isPrimeNum = false;
+            boolean isPrimeNum = true;
             int readData = Integer.parseInt(read[j]);
             for (int k = 2; k < readData; k++) {
                 if (readData % k == 0) {
-                    isPrimeNum = true;
+                    isPrimeNum = false;
                     break;
                 }
             }
-            if (readData!=1&&!isPrimeNum)
+            if (readData!=1&&isPrimeNum)
                 count++;
         }
         System.out.println(count);
