37b927ba48fc402c8ac9fc895eca2f0bd74c40de
potionk <48196324+potionk@users.noreply.github.com>
2019-12-31 15:02:25 +0900

    add 2902 solution

--- src/bj2902/Main.java
+++ src/bj2902/Main.java
@@ -1,8 +1,17 @@
 package bj2902;
 
-public class Main {
-}
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-/*
-Knuth-Morris-Pratt
- */
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input=br.readLine().split("-");
+        StringBuilder sb=new StringBuilder();
+        for(int i=0; i<input.length; i++){
+            sb.append(input[i].charAt(0));
+        }
+        System.out.println(sb.toString());
+    }
+}
