55359ceec28124f346eb67ea0c734a24d339336b
potionk <48196324+potionk@users.noreply.github.com>
2020-01-01 13:00:25 +0900

    add 10808 solution

---
+++ src/bj10808/Main.java
@@ -0,0 +1,77 @@
+package bj10808;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input=br.readLine();
+        int[] alphabet=new int[26];
+        for(int i=0; i<input.length(); i++){
+            alphabet[findIndex(input.charAt(i))]++;
+        }
+        for(int i=0; i<alphabet.length; i++){
+            System.out.print(alphabet[i]+" ");
+        }
+    }
+
+    static int findIndex(char input){
+        switch (input){
+            case 'a':
+                return 0;
+            case 'b':
+                return 1;
+            case 'c':
+                return 2;
+            case 'd':
+                return 3;
+            case 'e':
+                return 4;
+            case 'f':
+                return 5;
+            case 'g':
+                return 6;
+            case 'h':
+                return 7;
+            case 'i':
+                return 8;
+            case 'j':
+                return 9;
+            case 'k':
+                return 10;
+            case 'l':
+                return 11;
+            case 'm':
+                return 12;
+            case 'n':
+                return 13;
+            case 'o':
+                return 14;
+            case 'p':
+                return 15;
+            case 'q':
+                return 16;
+            case 'r':
+                return 17;
+            case 's':
+                return 18;
+            case 't':
+                return 19;
+            case 'u':
+                return 20;
+            case 'v':
+                return 21;
+            case 'w':
+                return 22;
+            case 'x':
+                return 23;
+            case 'y':
+                return 24;
+            case 'z':
+                return 25;
+        }
+        return -1;
+    }
+}
