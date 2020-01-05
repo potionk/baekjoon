c812c6e101ccb3e01fb79f8e9b11fc0b3f707ef0
potionk <48196324+potionk@users.noreply.github.com>
2020-01-05 12:52:15 +0900

    add 4504 solution

---
+++ src/bj4504/Main.java
@@ -0,0 +1,29 @@
+package bj4504;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static int num;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        num=Integer.parseInt(br.readLine());
+        int input;
+        while((input=Integer.parseInt(br.readLine()))!=0){
+            System.out.println(input+(isMultiple(input)?" is a multiple of ":" is NOT a multiple of ")+num+".");
+        }
+    }
+    public static boolean isMultiple(int input){
+        return input % num == 0;
+    }
+}
+/*
+3
+1
+7
+99
+321
+777
+0
+ */
