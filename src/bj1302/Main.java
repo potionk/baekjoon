8a4ac40ff0e84d911325ae3d7d41a0f8312e255f
potionk <48196324+potionk@users.noreply.github.com>
2020-01-05 15:08:54 +0900

    add 1302 solution

--- src/bj1302/Main.java
+++ src/bj1302/Main.java
@@ -4,27 +4,32 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int num=Integer.parseInt(br.readLine());
+        Map<String, Integer> data = new HashMap<>();
         String[] soldBooks=new String[num];
         for(int i=0; i<num; i++){
-            soldBooks[i]=br.readLine();
+            String input=br.readLine();
+            soldBooks[i]=input;
+            data.merge(input, 1, Integer::sum);
         }
         Arrays.sort(soldBooks);
+        String previousBook="";
         String bestSeller="";
-        String previousBook=soldBooks[num-1];
-        int bestSellerCount=-1;
-        int count=0;
-        for(int i=num-1; i>=0; i--){
-            if(!(previousBook.equals(soldBooks[i])))
-                count=0;
-            if(++count>=bestSellerCount){
-                bestSellerCount=count;
+        int bestSellerCount=0;
+        for(int i=soldBooks.length-1; i>=0; i--){
+            if(previousBook.equals(soldBooks[i]))
+                continue;
+            if(data.get(soldBooks[i])>=bestSellerCount){
+                bestSellerCount=data.get(soldBooks[i]);
                 bestSeller=soldBooks[i];
             }
+            previousBook=soldBooks[i];
         }
         System.out.println(bestSeller);
     }
