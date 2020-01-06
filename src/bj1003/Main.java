84b8ab86a93ceb94b1832a1f3c1d7bcb3e960602
potionk <48196324+potionk@users.noreply.github.com>
2020-01-06 19:25:40 +0900

    fix 1003 solution

--- src/bj1003/Main.java
+++ src/bj1003/Main.java
@@ -5,27 +5,25 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    static int zeroCount=0;
-    static int oneCount=0;
+    static int[] fiboZero=new int[41];
+    static int[] fiboOne=new int[41];
     public static void main(String[] args) throws IOException {
+        init();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int size=Integer.parseInt(br.readLine());
         for(int i=0; i<size; i++){
-            fibonacci(Integer.parseInt(br.readLine()));
-            System.out.println(zeroCount+" "+oneCount);
-            zeroCount=0;
-            oneCount=0;
+            int input=Integer.parseInt(br.readLine());
+            System.out.println(fiboZero[input]+" "+fiboOne[input]);
         }
     }
-    public static int fibonacci(int n) {
-        if (n == 0) {
-            zeroCount++;
-            return 0;
-        } else if (n == 1) {
-            oneCount++;
-            return 1;
-        } else {
-            return fibonacci(n-1) + fibonacci(n-2);
+    public static void init() {
+        fiboZero[0] = 1;
+        fiboOne[0] = 0;
+        fiboZero[1] = 0;
+        fiboOne[1] = 1;
+        for (int i=2; i<=40; i++) {
+            fiboZero[i]=fiboZero[i-1]+fiboZero[i-2];
+            fiboOne[i]=fiboOne[i-1]+fiboOne[i-2];
         }
     }
 }
