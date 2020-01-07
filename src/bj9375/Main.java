3cd37338499233cf240dc054f2436bd5b1340f4b
potionk <48196324+potionk@users.noreply.github.com>
2020-01-07 21:25:54 +0900

    add 9375 solution

--- src/bj9375/Main.java
+++ src/bj9375/Main.java
@@ -4,49 +4,31 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.Map;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Map<String, LinkedList<String>> closet=new HashMap<>();
+        Map<String, Integer> closet=new HashMap<>();
         int num = Integer.parseInt(br.readLine());
         for(int i=0; i<num; i++){
-            int result=0;
+            int result=1;
             int clothes=Integer.parseInt(br.readLine());
             for(int j=0; j<clothes; j++){
                 String[] readClothes=br.readLine().split(" ");
-                String clothesName=readClothes[0];
                 String clothesClass=readClothes[1];
                 if(closet.get(clothesClass)==null){
-                    LinkedList<String> list=new LinkedList<>();
-                    list.add(clothesName);
-                    closet.put(clothesClass,list);
+                    closet.put(clothesClass,1);
                 } else {
-                    LinkedList<String> list=closet.get(clothesClass);
-                    list.add(clothesName);
-                    closet.put(clothesClass,list);
+                    int res=closet.get(clothesClass);
+                    closet.put(clothesClass,++res);
                 }
             }
-            for(int j=1; j<closet.size(); j++){
-
+            for (String key : closet.keySet()) {
+                result*=closet.get(key)+1;
             }
-            result+=closet.size();
-            System.out.println(result);
+            System.out.println(result-1);
             closet.clear();
         }
     }
 }
-
-/*
-2
-3
-hat headgear
-sunglasses eyewear
-turban headgear
-3
-mask face
-sunglasses face
-makeup face
- */
