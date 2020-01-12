416ce4ad46dc13569af112df90cb640b21ca1fc3
potionk <48196324+potionk@users.noreply.github.com>
2020-01-12 14:51:45 +0900

    add 10926 solution

---
+++ src/bj10926/Main.java
@@ -0,0 +1,13 @@
+package bj10926;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb=new StringBuilder();
+        sb.append(br.readLine());
+        sb.append("??!");
+        System.out.println(sb.toString());
+    }
+}
