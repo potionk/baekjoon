6859c46c97b867b20b97c48376789fe47c61d434
potionk <48196324+potionk@users.noreply.github.com>
2020-01-16 19:42:41 +0900

    add 9935 solution

--- src/bj9935/Main.java
+++ src/bj9935/Main.java
@@ -3,72 +3,41 @@ package bj9935;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Stack;
 
 public class Main {
-    static char[] bomb;
-    static boolean[] liveStr;
-    static char[] str;
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        str=br.readLine().toCharArray();
-        bomb=br.readLine().toCharArray();
-        liveStr=new boolean[str.length];
-
-        boolean found;
-        do {
-            if (find()) {
-                found = true;
-            } else {
-                found = false;
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        Stack<Character> stack=new Stack<>();
+        char[] input=br.readLine().toCharArray();
+        int inputSize=input.length;
+        char[] bomb=br.readLine().toCharArray();
+        int bombSize=bomb.length;
+        for(int i=inputSize-1; i>=0; i--){
+            stack.push(input[i]);
+            int stackSize=stack.size();
+            if(stackSize>=bombSize){
+                boolean isBomb=true;
+                for(int j=stackSize-bombSize, bombIndex=bombSize-1; j<stackSize; j++, bombIndex--){
+                    if(stack.get(j)!=bomb[bombIndex]){
+                        isBomb=false;
+                        break;
+                    }
+                }
+                if(isBomb){
+                    for(int j=0; j<bombSize; j++){
+                        stack.pop();
+                    }
+                }
             }
-        } while(found);
-        if(str.length==0)
+        }
+        if(stack.isEmpty())
             System.out.println("FRULA");
         else{
-            boolean isEmpty=true;
-            for(int i=0; i<str.length; i++){
-                if(liveStr[i])
-                    continue;
-                System.out.print(str[i]);
-                isEmpty=false;
-            }
-            if(isEmpty)
-                System.out.println("FRULA");
-        }
-    }
-    public static boolean find(){
-        boolean isBomb=false;
-        int[] bombIndex=new int[bomb.length];
-        for(int i=0; i<str.length-bomb.length+1; i++){
-            int t=0;
-            if(liveStr[i])
-                continue;
-            isBomb=true;
-            for(int j=i; j<bomb.length+i+t; j++){
-                while(liveStr[j]){
-                    j++;
-                    t++;
-                    if(j>liveStr.length-bomb.length+(j-i))
-                        break;
-                }
-                if(j>liveStr.length-bomb.length+(j-i))
-                    break;
-                if(str[j]!=bomb[j-i-t]){
-                    isBomb=false;
-                    break;
-                }
-                bombIndex[j-i-t]=j;
-            }
-            if(isBomb){
-                for(int j=0; j<bombIndex.length; j++){
-                    liveStr[bombIndex[j]]=true;
-                }
-                break;
+            int stackSize=stack.size();
+            for(int i=0; i<stackSize; i++){
+                System.out.print(stack.pop());
             }
         }
-        if(isBomb)
-            return true;
-        else
-            return false;
     }
 }
