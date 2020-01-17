37d8c555c87744da8f4bd44898dd8f05a184b00a
potionk <48196324+potionk@users.noreply.github.com>
2020-01-17 20:01:09 +0900

    add 1436 solution

--- src/bj1436/Main.java
+++ src/bj1436/Main.java
@@ -3,17 +3,31 @@ package bj1436;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.List;
 
 public class Main {
-    // todo 다시 풀어보기
-    public static void main(String[] args) throws IOException {
+    public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int input = Integer.parseInt(br.readLine());
-        List<Integer> list=new LinkedList<>();
-        for(int i=0; i<10000; i++){
-
+        int testCase= Integer.parseInt(br.readLine());
+        int six=666;
+        int count=1;
+        if(testCase==1){
+            System.out.println(six);
+            return;
+        }
+        while(true){
+            six++;
+            String s2Str=six+"";
+            int strLen=s2Str.length();
+            for(int i=0; i<strLen-2; i++){
+                if(s2Str.charAt(i)=='6'&&s2Str.charAt(i+1)=='6'&&s2Str.charAt(i+2)=='6'){
+                    count++;
+                    if(count==testCase){
+                        System.out.println(six);
+                        return;
+                    }
+                    break;
+                }
+            }
         }
     }
 }
