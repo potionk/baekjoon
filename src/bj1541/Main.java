d7a021a954d0b0512430f32782d171ba9420137f
potionk <48196324+potionk@users.noreply.github.com>
2020-01-20 18:48:54 +0900

    add 1541 solution

--- src/bj1541/Main.java
+++ src/bj1541/Main.java
@@ -8,27 +8,16 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] testCase=br.readLine().split("-");
+        String[] tcFirst= testCase[0].split("\\+");
         int total=0;
-        boolean isExistMinus=false;
-        if(testCase.length>1){
-            total= Integer.parseInt(testCase[0]);
-            isExistMinus=true;
+        for(int j=0; j<tcFirst.length; j++){
+            total+= Integer.parseInt(tcFirst[j]);
         }
-        int i;
-        if(isExistMinus)
-            i=1;
-        else
-            i=0;
-        for(; i<testCase.length; i++){
-            String[] subTotal=testCase[i].split("\\+");
-            int t=0;
-            for(int j=0; j<subTotal.length; j++){
-                t+= Integer.parseInt(subTotal[j]);
+        for(int i=1; i<testCase.length; i++){
+            String[] sub=testCase[i].split("\\+");
+            for(int j=0; j<sub.length; j++){
+                total-= Integer.parseInt(sub[j]);
             }
-            if(!isExistMinus)
-                total=t;
-            else
-                total-=t;
         }
         System.out.println(total);
     }
