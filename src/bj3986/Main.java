3d9fe33b80aef06927b86a30bd466c8496c712c6
potionk <48196324+potionk@users.noreply.github.com>
2020-01-29 21:26:46 +0900

    add 3986 solution

--- src/bj3986/Main.java
+++ src/bj3986/Main.java
@@ -1,14 +1,12 @@
 package bj3986;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.Stack;
 
 public class Main {
-    static char[] testCase;
     public static void main(String[] args) throws IOException {
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
         int tc= Integer.parseInt(br.readLine());
         int result=0;
         for(int i=0; i<tc; i++){
@@ -26,6 +24,7 @@ public class Main {
             if(stack.size()==0)
                 result++;
         }
-        System.out.println(result);
+        bw.write(result+"");
+        bw.flush();
     }
 }
