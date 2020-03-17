c8d6af78ff8ffd231275dd868770ea1153a5f0c0
potionk <48196324+potionk@users.noreply.github.com>
2020-03-17 11:43:14 +0900

    Try 1931 problem
    TL

--- python/bj1931.py
+++ python/bj1931.py
@@ -11,26 +11,24 @@ finish_time = 0
 count = 0
 while True:
     find_next = sorted(meeting_room, key=lambda room: room[1])
-    print(find_next)
     finish_time = find_next[0][1]
-    print(finish_time)
-    # plus_count = 0
     flag = False
     while True:
         if len(meeting_room) <= 0:
             break
-        if meeting_room[0][0] < finish_time and meeting_room[0][0] == meeting_room[0][1]:
-            if meeting_room[0][1] < finish_time and flag:
+        if meeting_room[0][0] < finish_time:
+            if meeting_room[0][0] == meeting_room[0][1]:
                 count += 1
+            elif meeting_room[0][1] == finish_time:
+                count += 1
+                meeting_room.pop(0)
+                continue
             meeting_room.pop(0)
-            flag = True
-        elif meeting_room[0][0] < finish_time:
+        elif meeting_room[0][1] == finish_time:
+            count += 1
             meeting_room.pop(0)
-            flag = True
         else:
             break
-        print("count : " + str(count))
-    count = count + 1
     if len(meeting_room) <= 0:
         break
 
