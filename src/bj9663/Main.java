1d16f96afaae40079bf025d58b3acd2f40b99a72
potionk <48196324+potionk@users.noreply.github.com>
2020-03-27 08:36:11 +0900

    Add 9663 solution

--- src/bj9663/Main.java
+++ src/bj9663/Main.java
@@ -1,104 +1,42 @@
 package bj9663;
 
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    static boolean[][] chessField;
-    static int size, queen, available;
+    static int available;
+    static int size;
+    static int[] chessField;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         size = Integer.parseInt(br.readLine());
-        queen = size;
-        chessField = new boolean[size][size];
-        available=0;
-
-        for (int i = 0; i < size; i++) {
-            ArrayList<XY> list=calAttRange(i,0);
-            for(int j=0; j<list.size(); j++){
-                XY xy=list.get(j);
-                chessField[xy.getY()][xy.getX()]=true;
-            }
-            dfs(i, 0, 1);
-            for(int j=0; j<list.size(); j++){
-                XY xy=list.get(j);
-                chessField[xy.getY()][xy.getX()]=false;
-            }
-        }
+        available = 0;
+        chessField = new int[size];
+        dfs(0);
         System.out.println(available);
     }
 
-    public static void dfs(int x, int y, int count) {
-        for (int i = 0; i < size; i++) {
-            if(count==size-1){
-                if(!chessField[y+1][i]){
-                    available++;
-                }
-                return;
-            }
-            else if(!chessField[y+1][i]){
-                ArrayList<XY> list=calAttRange(i,y+1);
-                for(int j=0; j<list.size(); j++){
-                    XY xy=list.get(j);
-                    chessField[xy.getY()][xy.getX()]=true;
-                }
-                dfs(i, y+1, count+1);
-                for(int j=0; j<list.size(); j++){
-                    XY xy=list.get(j);
-                    chessField[xy.getY()][xy.getX()]=false;
-                }
-            }
-
-        }
-    }
-
-    public static ArrayList<XY> calAttRange(int x, int y) {
-        ArrayList<XY> list = new ArrayList<>();
-        if(!chessField[y][x])
-            list.add(new XY(y,x));
-        for (int j = 0; j < size; j++) {
-            if(!chessField[y][j]&&j!=x)
-                list.add(new XY(j, y));
-            if(!chessField[j][x]&&j!=y)
-                list.add(new XY(x, j));
-        }
-        int cursorX, cursorY;
-        int[] nextX = {-1, 1, -1, 1};
-        int[] nextY = {-1, -1, 1, 1};
-        for (int i = 0; i < 4; i++) {
-            cursorX = x;
-            cursorY = y;
-            while (true) {
-                if (cursorX + nextX[i] >= 0 && cursorX + nextX[i] < size && cursorY + nextY[i] >= 0 && cursorY + nextY[i] < size){
-                    if(!chessField[cursorY + nextY[i]][cursorX + nextX[i]]){
-                        list.add(new XY(cursorX + nextX[i], cursorY + nextY[i]));
-                    }
-                    cursorX += nextX[i];
-                    cursorY += nextY[i];
-                }
-                else
-                    break;
+    public static void dfs(int x) {
+        if (x == size) {
+            available++;
+        } else {
+            for (int i = 0; i < size; i++) {
+                int temp = chessField[x];
+                chessField[x] = i;
+                if (isAvailable(x))
+                    dfs(x + 1);
+                chessField[x] = temp;
             }
         }
-        return list;
-    }
-}
-
-class XY {
-    int x;
-    int y;
-
-    public XY(int x, int y) {
-        this.x = x;
-        this.y = y;
     }
 
-    public int getX() {
-        return x;
-    }
-
-    public int getY() {
-        return y;
+    public static boolean isAvailable(int x) {
+        for (int i = 0; i < x; i++) {
+            if (chessField[i] == chessField[x] || Math.abs(chessField[i] - chessField[x]) == Math.abs(x - i))
+                return false;
+        }
+        return true;
     }
 }
