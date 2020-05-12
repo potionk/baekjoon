9f7c8b64b934100bed767a7a9304c477456d47a0
potionk <48196324+potionk@users.noreply.github.com>
2020-05-12 11:31:35 +0900

    Refactor 7576 solution

--- src/bj7576/Main.java
+++ src/bj7576/Main.java
@@ -11,85 +11,77 @@ public class Main {
     static int count;
     static int xSize, ySize;
     static int totalNonMinusOne;
+    static int[] xArrow, yArrow;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] testCase=br.readLine().split(" ");
-        xSize= Integer.parseInt(testCase[0]);
-        ySize= Integer.parseInt(testCase[1]);
-        ArrayList<XY> list=new ArrayList<>();
-        count=0;
-        int nonMinusOne=0;
-        totalNonMinusOne=0;
-        graph=new int[ySize][xSize];
-        for(int i=0; i<ySize; i++){
-            String[] input=br.readLine().split(" ");
-            for(int j=0; j<input.length; j++){
-                graph[i][j]= Integer.parseInt(input[j]);
-                if (graph[i][j] == 1){
-                    list.add(new XY(j,i));
+        String[] testCase = br.readLine().split(" ");
+        xSize = Integer.parseInt(testCase[0]);
+        ySize = Integer.parseInt(testCase[1]);
+        xArrow = new int[]{0, 1, 0, -1};
+        yArrow = new int[]{1, 0, -1, 0};
+        ArrayList<XY> list = new ArrayList<>();
+        count = 0;
+        int nonMinusOne = 0;
+        totalNonMinusOne = 0;
+        graph = new int[ySize][xSize];
+        for (int i = 0; i < ySize; i++) {
+            String[] input = br.readLine().split(" ");
+            for (int j = 0; j < input.length; j++) {
+                graph[i][j] = Integer.parseInt(input[j]);
+                if (graph[i][j] == 1) {
+                    list.add(new XY(j, i));
                     totalNonMinusOne++;
                 }
-                if(graph[i][j]!=-1)
+                if (graph[i][j] != -1)
                     nonMinusOne++;
             }
         }
         bfs(list);
-        if(totalNonMinusOne!=nonMinusOne){
+        if (totalNonMinusOne != nonMinusOne)
             System.out.println(-1);
-            return;
-        }
-        System.out.println(count);
+        else
+            System.out.println(count);
     }
-    public static void bfs(ArrayList<XY> list){
+
+    public static void bfs(ArrayList<XY> list) {
         LinkedList<XY> queue = new LinkedList<>(list);
-        boolean isContinue=true;
-        while(isContinue){
-            isContinue=false;
-            int beforeQueueSize=queue.size();
-            for(int i=0; i<beforeQueueSize; i++){
-                XY next=queue.poll();
-                int x=next.getX();
-                int y=next.getY();
-                if(!(y+1>=ySize)&&graph[y+1][x]==0){
-                    queue.add(new XY(x,y+1));
-                    graph[y+1][x]=1;
-                    isContinue=true;
-                    totalNonMinusOne++;
-                }
-                if(!(x+1>=xSize)&&graph[y][x+1]==0){
-                    queue.add(new XY(x+1,y));
-                    graph[y][x+1]=1;
-                    isContinue=true;
-                    totalNonMinusOne++;
-                }
-                if(!(y-1<0)&&graph[y-1][x]==0){
-                    queue.add(new XY(x,y-1));
-                    graph[y-1][x]=1;
-                    isContinue=true;
-                    totalNonMinusOne++;
-                }
-                if(!(x-1<0)&&graph[y][x-1]==0){
-                    queue.add(new XY(x-1,y));
-                    graph[y][x-1]=1;
-                    isContinue=true;
-                    totalNonMinusOne++;
+        boolean isContinue = true;
+        while (isContinue) {
+            isContinue = false;
+            int beforeQueueSize = queue.size();
+            for (int i = 0; i < beforeQueueSize; i++) {
+                XY next = queue.poll();
+                int x = next.getX();
+                int y = next.getY();
+                for (int a = 0; a < 4; a++) {
+                    if (!(x + xArrow[a] < 0) && !(y + yArrow[a] < 0) && !(x + xArrow[a] >= xSize) && !(y + yArrow[a] >= ySize) && graph[y + yArrow[a]][x + xArrow[a]] == 0) {
+                        queue.add(new XY(x + xArrow[a], y + yArrow[a]));
+                        graph[y + yArrow[a]][x + xArrow[a]] = 1;
+                        isContinue = true;
+                        totalNonMinusOne++;
+                    }
                 }
             }
-            if(isContinue)
+            if (isContinue)
                 count++;
         }
     }
 }
-class XY{
+
+class XY {
     int x;
     int y;
+
     public XY(int x, int y) {
         this.x = x;
         this.y = y;
     }
+
     public int getX() {
         return x;
     }
+
     public int getY() {
         return y;
     }
