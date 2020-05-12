e0e595f6849826f4c406a2585d68268cb21e3a70
potionk <48196324+potionk@users.noreply.github.com>
2020-05-12 12:10:45 +0900

    Refactor 2667 solution

--- src/bj2667/Main.java
+++ src/bj2667/Main.java
@@ -13,44 +13,45 @@ public class Main {
     static int size;
     static List<Integer> result;
     static int currentCount;
+    static int[] xArrow, yArrow;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        size= Integer.parseInt(br.readLine());
-        graph=new int[size][size];
-        dfsIsVisited=new boolean[size][size];
-        result=new ArrayList<>();
-        currentCount=0;
-        for(int i=0; i<size; i++){
-            String input=br.readLine();
-            for(int j=0; j<size; j++){
-                graph[i][j]= Integer.parseInt(input.charAt(j)+"");
+        size = Integer.parseInt(br.readLine());
+        graph = new int[size][size];
+        dfsIsVisited = new boolean[size][size];
+        result = new ArrayList<>();
+        currentCount = 0;
+        xArrow = new int[]{0, 1, 0, -1};
+        yArrow = new int[]{1, 0, -1, 0};
+        for (int i = 0; i < size; i++) {
+            String input = br.readLine();
+            for (int j = 0; j < size; j++) {
+                graph[i][j] = Integer.parseInt(input.charAt(j) + "");
             }
         }
-        for(int i=0; i<size; i++){
-            for(int j=0; j<size; j++){
-                if(graph[i][j]==1&&!dfsIsVisited[i][j]){
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (graph[i][j] == 1 && !dfsIsVisited[i][j]) {
                     dfs(i, j);
                     result.add(currentCount);
-                    currentCount=0;
+                    currentCount = 0;
                 }
             }
         }
         System.out.println(result.size());
         Collections.sort(result);
-        for(int i=0; i<result.size(); i++){
-            System.out.println(result.get(i));
+        for (Integer integer : result) {
+            System.out.println(integer);
         }
     }
-    public static void dfs(int y, int x){
-        dfsIsVisited[y][x]=true;
+
+    public static void dfs(int y, int x) {
+        dfsIsVisited[y][x] = true;
         currentCount++;
-        if(!(y-1<0)&&graph[y-1][x]==1&&!dfsIsVisited[y-1][x])
-            dfs(y-1,x);
-        if(!(y+1>=size)&&graph[y+1][x]==1&&!dfsIsVisited[y+1][x])
-            dfs(y+1,x);
-        if(!(x-1<0)&&graph[y][x-1]==1&&!dfsIsVisited[y][x-1])
-            dfs(y,x-1);
-        if(!(x+1>=size)&&graph[y][x+1]==1&&!dfsIsVisited[y][x+1])
-            dfs(y,x+1);
+        for (int i = 0; i < 4; i++) {
+            if (!(x + xArrow[i] >= size) && !(x + xArrow[i] < 0) && !(y + yArrow[i] >= size) && !(y + yArrow[i] < 0) && graph[y + yArrow[i]][x + xArrow[i]] == 1 && !dfsIsVisited[y + yArrow[i]][x + xArrow[i]])
+                dfs(y + yArrow[i], x + xArrow[i]);
+        }
     }
 }
