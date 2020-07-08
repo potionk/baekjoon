12246a596857681629e0773c0e52659a8bfd120c
potionk <potionkr@gmail.com>
2020-07-08 19:15:26 +0900

    Refactor 2178 solution

--- src/bj2178/Main.java
+++ src/bj2178/Main.java
@@ -10,65 +10,61 @@ public class Main {
     static boolean[][] bfsIsVisited;
     static int xSize, ySize;
     static int[][] count;
+    static int[] xArrow, yArrow;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] testCase=br.readLine().split(" ");
-        xSize= Integer.parseInt(testCase[1]);
-        ySize= Integer.parseInt(testCase[0]);
-        graph=new int[ySize][xSize];
-        count=new int[ySize][xSize];
-        for(int i=0; i<ySize; i++){
-            String input=br.readLine();
-            for(int j=0; j<input.length(); j++){
-                graph[i][j]= Integer.parseInt(input.charAt(j)+"");
+        String[] size = br.readLine().split(" ");
+        xSize = Integer.parseInt(size[1]);
+        ySize = Integer.parseInt(size[0]);
+        graph = new int[ySize][xSize];
+        count = new int[ySize][xSize];
+        xArrow = new int[]{1, 0, -1, 0};
+        yArrow = new int[]{0, -1, 0, 1};
+        for (int i = 0; i < ySize; i++) {
+            String input = br.readLine();
+            for (int j = 0; j < xSize; j++) {
+                graph[i][j] = Integer.parseInt(input.charAt(j) + "");
             }
         }
-        bfsIsVisited=new boolean[ySize][xSize];
-        bfs(0,0);
-        System.out.println(count[ySize-1][xSize-1]);
+        bfsIsVisited = new boolean[ySize][xSize];
+        bfs(0, 0);
+        System.out.println(count[ySize - 1][xSize - 1]);
     }
-    public static void bfs(int y, int x){
-        LinkedList<XY> queue=new LinkedList<>();
-        queue.add(new XY(x,y));
-        bfsIsVisited[y][x]=true;
-        count[y][x]=1;
-        while(queue.size()!=0){
-            XY next=queue.poll();
-            x=next.getX();
-            y=next.getY();
-            if(!(next.y+1>=ySize)&&graph[y+1][x]==1&&!bfsIsVisited[y+1][x]){
-                queue.add(new XY(x,y+1));
-                bfsIsVisited[y+1][x]=true;
-                count[y+1][x]=count[y][x]+1;
-            }
-            if(!(x+1>=xSize)&&graph[y][x+1]==1&&!bfsIsVisited[y][x+1]){
-                queue.add(new XY(x+1,y));
-                bfsIsVisited[y][x+1]=true;
-                count[y][x+1]=count[y][x]+1;
-            }
-            if(!(y-1<0)&&graph[y-1][x]==1&&!bfsIsVisited[y-1][x]){
-                queue.add(new XY(x,y-1));
-                bfsIsVisited[y-1][x]=true;
-                count[y-1][x]=count[y][x]+1;
-            }
-            if(!(x-1<0)&&graph[y][x-1]==1&&!bfsIsVisited[y][x-1]){
-                queue.add(new XY(x-1,y));
-                bfsIsVisited[y][x-1]=true;
-                count[y][x-1]=count[y][x]+1;
+
+    public static void bfs(int y, int x) {
+        LinkedList<XY> queue = new LinkedList<>();
+        queue.add(new XY(x, y));
+        bfsIsVisited[y][x] = true;
+        count[y][x] = 1;
+        while (queue.size() != 0) {
+            XY next = queue.poll();
+            x = next.getX();
+            y = next.getY();
+            for (int i = 0; i < 4; i++) {
+                if (!(next.y + yArrow[i] >= ySize) && !(x + xArrow[i] >= xSize) && !(y + yArrow[i] < 0) && !(x + xArrow[i] < 0) && graph[y + yArrow[i]][x + xArrow[i]] == 1 && !bfsIsVisited[y + yArrow[i]][x + xArrow[i]]) {
+                    queue.add(new XY(x + xArrow[i], y + yArrow[i]));
+                    bfsIsVisited[y + yArrow[i]][x + xArrow[i]] = true;
+                    count[y + yArrow[i]][x + xArrow[i]] = count[y][x] + 1;
+                }
             }
         }
     }
 }
-class XY{
+
+class XY {
     int x;
     int y;
+
     public XY(int x, int y) {
         this.x = x;
         this.y = y;
     }
+
     public int getX() {
         return x;
     }
+
     public int getY() {
         return y;
     }
