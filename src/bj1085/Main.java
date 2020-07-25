0fb2744edfb675f5443c5d8af87f0104b46a774c
JongunKim <potionkr@gmail.com>
2020-07-25 09:48:55 +0900

    Refactor 1085 solution

--- src/bj1085/Main.java
+++ src/bj1085/Main.java
@@ -3,7 +3,6 @@ package bj1085;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -14,7 +13,13 @@ public class Main {
         result[1]=Integer.parseInt(input[1]);
         result[2]=Integer.parseInt(input[2])-result[0];
         result[3]=Integer.parseInt(input[3])-result[1];
-        Arrays.sort(result);
-        System.out.println(result[0]);
+        System.out.println(findMin(result));
+    }
+    public static int findMin(int[] arr){
+        int min= Integer.MAX_VALUE;
+        for(int i=0; i<arr.length; i++){
+            min=Math.min(min, arr[i]);
+        }
+        return min;
     }
 }
