9f45d0fb05834fe060ec67a013ab54a5f3a16845
JongunKim <potionkr@gmail.com>
2020-07-25 09:49:12 +0900

    Refactor 2446 solution

--- src/bj2446/Main.java
+++ src/bj2446/Main.java
@@ -1,36 +1,30 @@
 package bj2446;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int testCase = Integer.parseInt(br.readLine());
-        for(int i=testCase*2-1, blank=0; i>0; i-=2, blank++){
-            System.out.print(makeBlank(blank));
-            System.out.println(makeStar(i));
+        for (int i = testCase * 2 - 1, blank = 0; i > 0; i -= 2, blank++) {
+            for (int b = 0; b < blank; b++) {
+                bw.write(" ");
+            }
+            for (int s = 0; s < i; s++) {
+                bw.write("*");
+            }
+            bw.write("\n");
         }
-        for(int i=3, blank=testCase-2; i<testCase*2; i+=2, blank--){
-            System.out.print(makeBlank(blank));
-            System.out.println(makeStar(i));
+        for (int i = 3, blank = testCase - 2; i < testCase * 2; i += 2, blank--) {
+            for (int b = 0; b < blank; b++) {
+                bw.write(" ");
+            }
+            for (int s = 0; s < i; s++) {
+                bw.write("*");
+            }
+            bw.write("\n");
         }
-    }
-
-    public static String makeStar(int number) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < number; i++) {
-            sb.append("*");
-        }
-        return sb.toString();
-    }
-
-    public static String makeBlank(int number) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < number; i++) {
-            sb.append(" ");
-        }
-        return sb.toString();
+        bw.flush();
     }
 }
