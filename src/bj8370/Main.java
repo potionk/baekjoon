0c55d255be6a4a31db274236ce3135571adf0d2b
potionk <potionkr@gmail.com>
2020-08-14 13:34:09 +0900

    Add 8370 solution

---
+++ src/bj8370/Main.java
@@ -0,0 +1,11 @@
+package bj8370;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] tc = br.readLine().split(" ");
+        System.out.println(Integer.parseInt(tc[0]) * Integer.parseInt(tc[1]) + Integer.parseInt(tc[2]) * Integer.parseInt(tc[3]));
+    }
+}
