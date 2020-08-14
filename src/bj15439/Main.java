e59e54b2e16182f0907a22221d960b003ffbde40
potionk <potionkr@gmail.com>
2020-08-14 14:32:05 +0900

    Add 15439 solution

---
+++ src/bj15439/Main.java
@@ -0,0 +1,11 @@
+package bj15439;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine());
+        System.out.println(tc * (tc - 1));
+    }
+}
