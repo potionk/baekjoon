c5647fe79d49efa53bcbb79e695da7bac76b0213
potionk <potionkr@gmail.com>
2020-08-14 16:00:16 +0900

    Add 15894 solution

---
+++ src/bj15894/Main.java
@@ -0,0 +1,10 @@
+package bj15894;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(Long.parseLong(br.readLine()) * 4);
+    }
+}
