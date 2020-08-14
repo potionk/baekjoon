7704d8ba1f3f2defc1045eade9a319cf6bcc31b5
potionk <potionkr@gmail.com>
2020-08-14 16:46:11 +0900

    Add 18108 solution

---
+++ src/bj18108/Main.java
@@ -0,0 +1,10 @@
+package bj18108;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(Integer.parseInt(br.readLine())-543);
+    }
+}
