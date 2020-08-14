7d7758b65a729d1be62f2840dfb174d75a6d7e26
potionk <potionkr@gmail.com>
2020-08-14 16:12:38 +0900

    Add 16394 solution

---
+++ src/bj16394/Main.java
@@ -0,0 +1,10 @@
+package bj16394;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(Integer.parseInt(br.readLine()) - 1946);
+    }
+}
