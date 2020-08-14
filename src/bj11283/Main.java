c732d25321af59953db85bfcb365b4182b77a229
potionk <potionkr@gmail.com>
2020-08-14 13:40:34 +0900

    Add 11283 solution

---
+++ src/bj11283/Main.java
@@ -0,0 +1,11 @@
+package bj11283;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String tc=br.readLine();
+        System.out.println(tc.toCharArray()[0]-44031);
+    }
+}
