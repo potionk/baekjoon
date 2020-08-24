e293127e95e77825bfba0824bd0537d5e1a43fef
potionk <potionkr@gmail.com>
2020-08-24 22:33:58 +0900

    Add 4999 solution

---
+++ src/bj4999/Main.java
@@ -0,0 +1,12 @@
+package bj4999;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(br.readLine().length() >= br.readLine().length() ? "go" : "no");
+    }
+}
