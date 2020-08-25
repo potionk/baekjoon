48cafa06c4191cdae313f65640b59031a9bcf645
potionk <potionkr@gmail.com>
2020-08-25 13:59:49 +0900

    Add 9613 solution

---
+++ src/bj9613/Main.java
@@ -0,0 +1,25 @@
+package bj9613;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int total = 0;
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < 7; i++) {
+            int tc = Integer.parseInt(br.readLine());
+            if (tc % 2 == 1) {
+                total += tc;
+                min = Math.min(tc, min);
+            }
+        }
+        if (total != 0) {
+            System.out.println(total);
+            System.out.println(min);
+        } else {
+            System.out.println(-1);
+        }
+    }
+}
