ffe1c2d5f8e66ebe50d891aa2b12ed571c196487
potionk <potionkr@gmail.com>
2020-08-31 20:36:29 +0900

    Refactor 1158 solution

--- src/bj1158/Main.java
+++ src/bj1158/Main.java
@@ -1,34 +1,32 @@
 package bj1158;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] testCase=br.readLine().split(" ");
-        int N= Integer.parseInt(testCase[0]);
-        int K= Integer.parseInt(testCase[1]);
-        LinkedList<Integer> list=new LinkedList<>();
-        for(int i=1; i<=N; i++)
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String[] testCase = br.readLine().split(" ");
+        int N = Integer.parseInt(testCase[0]);
+        int K = Integer.parseInt(testCase[1]);
+        List<Integer> list = new ArrayList<>();
+        for (int i = 1; i <= N; i++) {
             list.add(i);
-        int index=-1;
-        System.out.print("<");
-        for(int i=0; i<N; i++){
-            for(int j=0; j<K; j++){
-                index++;
-                if(index==list.size())
-                    index=0;
-            }
-            int rem=list.remove(index);
-            index--;
-            if(i!=N-1)
-                System.out.print(rem+", ");
-            else
-                System.out.print(rem);
         }
-        System.out.print(">");
+        int index = -1;
+        bw.write("<");
+        for (int i = 0; i < N; i++) {
+            index = (index + K) % list.size();
+            int rem = list.remove(index);
+            index--;
+            if (i != N - 1)
+                bw.write(rem + ", ");
+            else
+                bw.write(rem + "");
+        }
+        bw.write(">");
+        bw.flush();
     }
 }
