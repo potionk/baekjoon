5b3683f9893b0821bd67738c079d8c01da97c703
potionk <potionkr@gmail.com>
2020-09-05 12:07:42 +0900

    Add 2445 solution

---
+++ src/bj2445/Main.java
@@ -0,0 +1,26 @@
+package bj2445;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int tc = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= tc; i++) {
+            bw.write(makeString(i, '*') + makeString(tc * 2 - i * 2, ' ') + makeString(i, '*') + "\n");
+        }
+        for (int i = tc-1; i > 0; i--) {
+            bw.write(makeString(i, '*') + makeString(tc * 2 - i * 2, ' ') + makeString(i, '*') + "\n");
+        }
+        bw.flush();
+    }
+
+    public static String makeString(int num, char c) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < num; i++) {
+            sb.append(c);
+        }
+        return sb.toString();
+    }
+}
