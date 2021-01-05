57d82e4af86e705b49900ad6166c2444b16c8db8
potionk <potionkr@gmail.com>
2021-01-05 19:51:39 +0900

    Add 1718 solution

---
+++ src/bj1718/Main.java
@@ -0,0 +1,21 @@
+package bj1718;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[] target = br.readLine().toCharArray();
+        char[] key = br.readLine().toCharArray();
+        for (int i = 0; i < target.length; i++) {
+            if (target[i] != ' ') {
+                target[i] = (char) ((target[i] - 97 - (key[i % key.length] - 97) + 25) % 26 + 97);
+            }
+        }
+        for (char c : target) {
+            System.out.print(c);
+        }
+    }
+}
