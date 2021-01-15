d38534eaa4681cff6f0e860aaf94656e98573f9d
potionk <potionkr@gmail.com>
2021-01-15 14:47:40 +0900

    Beautify 10989 solution

--- src/bj10989/Main.java
+++ src/bj10989/Main.java
@@ -7,27 +7,28 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int size = Integer.parseInt(br.readLine());
-        int MAX=10000000;
-        int[] input=new int[size];
-        int[] count=new int[MAX];
-        int[] cumSum=new int[MAX];
-        int[] result=new int[size];
-        int maxValue=-1;
-        for(int i=0; i<size; i++) {
+        int MAX = 10000000;
+        int[] input = new int[size];
+        int[] count = new int[MAX];
+        int[] cumSum = new int[MAX];
+        int[] result = new int[size];
+        int maxValue = -1;
+        for (int i = 0; i < size; i++) {
             input[i] = Integer.parseInt(br.readLine());
-            count[input[i]-1]++;
+            count[input[i] - 1]++;
             if (input[i] > maxValue)
                 maxValue = input[i];
         }
-        cumSum[0]=count[0];
-        for(int i=1; i<maxValue; i++){
-            cumSum[i]=cumSum[i-1]+count[i];
+        cumSum[0] = count[0];
+        for (int i = 1; i < maxValue; i++) {
+            cumSum[i] = cumSum[i - 1] + count[i];
         }
-        for(int i=0; i<size; i++){
-            result[--cumSum[input[i]-1]]=input[i];
+        for (int i = 0; i < size; i++) {
+            result[--cumSum[input[i] - 1]] = input[i];
         }
-        for(int i=0; i<size; i++)
-            bw.write(result[i]+"\n");
+        for (int i = 0; i < size; i++) {
+            bw.write(result[i] + "\n");
+        }
         bw.flush();
         bw.close();
     }
