bcc3c9c632be40ca8c045a5677930afc360027ef
potionk <potionkr@gmail.com>
2021-01-18 09:48:09 +0900

    Refactor 2751 solution

--- src/bj2751/Main.java
+++ src/bj2751/Main.java
@@ -1,21 +1,22 @@
 package bj2751;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String args[]) throws IOException {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int size = Integer.parseInt(br.readLine());
-        List<Integer> list=new LinkedList<>();
-        for(int i=0; i<size; i++)
+        List<Integer> list = new LinkedList<>();
+        for (int i = 0; i < size; i++)
             list.add(Integer.parseInt(br.readLine()));
         Collections.sort(list);
-        Object[] list2Array=list.toArray();
-        for(int i=0; i<size; i++){
-            System.out.println((int)list2Array[i]);
+        Object[] list2Array = list.toArray();
+        for (int i = 0; i < size; i++) {
+            bw.write(list2Array[i] + "\n");
         }
+        br.close();
+        bw.close();
     }
 }
