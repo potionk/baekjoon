b84b70066a5e0b9148cc489e2f6e56d4d476aa1e
potionk <potionkr@gmail.com>
2021-02-17 14:30:24 +0900

    Refactor 12018 solution

--- src/bj12018/Main.java
+++ src/bj12018/Main.java
@@ -8,30 +8,30 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String input[] = br.readLine().split(" ");
-        int n=Integer.parseInt(input[0]); //과목수
-        int m=Integer.parseInt(input[1]); //마일리지
-        int needM[]=new int[n]; //필요마일리지
-        int count=0;
-        for(int i=0; i<n; i++){
-            String in[] = br.readLine().split(" ");
-            int p=Integer.parseInt(in[0]); //신청한사람
-            int l=Integer.parseInt(in[1]); //수강인원
-            String mList[] = br.readLine().split(" ");
-            int itg[]=new int[mList.length];
-            for(int j=0; j<mList.length; j++){
-                itg[j]=Integer.parseInt(mList[j]);
+        String[] input = br.readLine().split(" ");
+        int n = Integer.parseInt(input[0]); //과목수
+        int m = Integer.parseInt(input[1]); //마일리지
+        int[] require = new int[n]; //필요마일리지
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            String[] in = br.readLine().split(" ");
+            int p = Integer.parseInt(in[0]); //신청한사람
+            int l = Integer.parseInt(in[1]); //수강인원
+            String[] mList = br.readLine().split(" ");
+            int[] itg = new int[mList.length];
+            for (int j = 0; j < mList.length; j++) {
+                itg[j] = Integer.parseInt(mList[j]);
             }
             Arrays.sort(itg);
-            if(p-l<0)
-                needM[i]=1;
+            if (p - l < 0)
+                require[i] = 1;
             else
-                needM[i]=itg[p-l];
+                require[i] = itg[p - l];
         }
-        Arrays.sort(needM);
-        for(int i=0; i<needM.length; i++){
-            m-=needM[i];
-            if(m>=0)
+        Arrays.sort(require);
+        for (int j : require) {
+            m -= j;
+            if (m >= 0)
                 count++;
         }
         System.out.println(count);
