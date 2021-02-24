88d574c3f14fa7779089768aaeb07c1aa62b5423
potionk <potionkr@gmail.com>
2021-02-24 17:35:59 +0900

    Add 20232 solution

---
+++ src/bj20232/Main.java
@@ -0,0 +1,14 @@
+package bj20232;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String[] winner = {"ITMO", "SPbSU", "SPbSU", "ITMO", "ITMO", "SPbSU", "ITMO", "ITMO", "ITMO", "ITMO", "ITMO", "PetrSU, ITMO", "SPbSU", "SPbSU", "ITMO", "ITMO", "ITMO", "ITMO", "SPbSU", "ITMO", "ITMO", "ITMO", "ITMO", "SPbSU", "ITMO"};
+        bw.write(winner[Integer.parseInt(br.readLine()) - 1995] + "");
+        br.close();
+        bw.close();
+    }
+}
