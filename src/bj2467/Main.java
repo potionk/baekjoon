586f9bf828ccc4cb87417d43f00cb11dbb9e5a6d
potionk <potionkr@gmail.com>
2021-03-11 17:15:53 +0900

    Refactor 2467 solution

--- src/bj2467/Main.java
+++ src/bj2467/Main.java
@@ -12,28 +12,18 @@ public class Main {
         for (int i = 0; i < N; i++) {
             s[i] = Integer.parseInt(sStr[i]);
         }
-        int start = 0, end = N - 1, beforeSum = s[start] + s[end], sum = s[start] + s[end], resultA = s[0], resultB = s[end];
-        while (end - start >= 2) {
-            int left = beforeSum + s[start + 1] - s[start];
-            int right = beforeSum + s[end - 1] - s[end];
-            int absLeft = Math.abs(left);
-            int absRight = Math.abs(right);
-            if (absLeft < absRight) {
+        int start = 0, end = N - 1, sum = s[start] + s[end], resultA = s[0], resultB = s[end];
+        while (start < end) {
+            int thisSum = s[start] + s[end];
+            if (Math.abs(thisSum) < Math.abs(sum)) {
+                sum = thisSum;
+                resultA = s[start];
+                resultB = s[end];
+            }
+            if (thisSum < 0) {
                 start++;
-                beforeSum = left;
-                if (absLeft < Math.abs(sum)) {
-                    sum = left;
-                    resultA = s[start];
-                    resultB = s[end];
-                }
             } else {
                 end--;
-                beforeSum = right;
-                if (absRight < Math.abs(sum)) {
-                    sum = right;
-                    resultA = s[start];
-                    resultB = s[end];
-                }
             }
         }
         bw.write(resultA + " " + resultB);
