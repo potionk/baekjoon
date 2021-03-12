4e3fef42e9560937afc7d737602f1b5161f31f2c
potionk <potionkr@gmail.com>
2021-03-12 10:52:21 +0900

    Add 2470 solution

---
+++ src/bj2470/Main.java
@@ -0,0 +1,35 @@
+package bj2470;
+
+import java.io.*;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        String[] sStr = br.readLine().split(" ");
+        int[] s = new int[N];
+        for (int i = 0; i < N; i++) {
+            s[i] = Integer.parseInt(sStr[i]);
+        }
+        Arrays.sort(s);
+        int start = 0, end = N - 1, sum = s[start] + s[end], resultA = s[0], resultB = s[end];
+        while (start < end) {
+            int thisSum = s[start] + s[end];
+            if (Math.abs(thisSum) < Math.abs(sum)) {
+                sum = thisSum;
+                resultA = s[start];
+                resultB = s[end];
+            }
+            if (thisSum < 0) {
+                start++;
+            } else {
+                end--;
+            }
+        }
+        bw.write(resultA + " " + resultB);
+        br.close();
+        bw.close();
+    }
+}
