573dde54b566d0ae8c8e5d6ded711105c5b19309
potionk <potionkr@gmail.com>
2021-03-15 17:11:07 +0900

    Refactor 1874 solution

--- src/bj1874/Main.java
+++ src/bj1874/Main.java
@@ -6,65 +6,61 @@ import java.util.Stack;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb=new StringBuilder();
-        int number= Integer.parseInt(br.readLine());
-        Stack<Integer> stack=new Stack<>();
-        for(int i=number; i>=1; i--){
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        int number = Integer.parseInt(br.readLine());
+        Stack<Integer> stack = new Stack<>();
+        for (int i = number; i >= 1; i--) {
             stack.push(i);
         }
-        Stack<Integer> result=new Stack<>();
-        int[] testCase=new int[number];
-        for(int i=0; i<number; i++){
-            testCase[i]= Integer.parseInt(br.readLine());
+        Stack<Integer> result = new Stack<>();
+        int[] testCase = new int[number];
+        for (int i = 0; i < number; i++) {
+            testCase[i] = Integer.parseInt(br.readLine());
         }
-        int cursor=-1;
-        for(int i=0; i<number; i++){
-            int target=testCase[i];
-            if(target>cursor){
-                boolean isFinish=false;
-                while(!stack.isEmpty()){
-                    if(stack.peek()==target){
-                        isFinish=true;
+        int cursor = -1;
+        for (int i = 0; i < number; i++) {
+            int target = testCase[i];
+            boolean isFinish = false;
+            if (target > cursor) {
+                while (!stack.isEmpty()) {
+                    if (stack.peek() == target) {
+                        isFinish = true;
                     }
                     result.push(stack.pop());
-//                    bw.write("+\n");
                     sb.append("+\n");
-                    if(isFinish){
+                    if (isFinish) {
                         result.pop();
-//                        bw.write("-\n");
                         sb.append("-\n");
-                        if(!result.isEmpty())
-                            cursor=result.peek();
-                        else
-                            cursor=-1;
+                        if (!result.isEmpty()) {
+                            cursor = result.peek();
+                        } else {
+                            cursor = -1;
+                        }
                         break;
                     }
                 }
-                if(!isFinish){
-                    System.out.println("NO");
-                    return;
-                }
             } else {
-                boolean isFinish=false;
-                while(!result.isEmpty()&&!isFinish){
-                    if(result.peek()==target){
-                        isFinish=true;
+                while (!result.isEmpty() && !isFinish) {
+                    if (result.peek() == target) {
+                        isFinish = true;
                     }
                     result.pop();
-//                    bw.write("-\n");
                     sb.append("-\n");
-                    if(!result.isEmpty())
-                        cursor=result.peek();
-                    else
-                        cursor=-1;
-                }
-                if(!isFinish){
-                    System.out.println("NO");
-                    return;
+                    if (!result.isEmpty()) {
+                        cursor = result.peek();
+                    } else {
+                        cursor = -1;
+                    }
                 }
             }
+            if (!isFinish) {
+                System.out.println("NO");
+                return;
+            }
         }
-        System.out.print(sb.toString());
+        bw.write(sb.toString());
+        bw.close();
+        br.close();
     }
 }
