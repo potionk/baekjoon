42a90ff93d0a2a9afee5361d5ba87baf1cb98024
potionk <potionkr@gmail.com>
2021-09-10 14:46:28 +0900

    Add 1766 solution

---
+++ src/bj1766/Main.java
@@ -0,0 +1,71 @@
+package bj1766;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String[] tc = br.readLine().split(" ");
+        Set<Integer> isDeleted = new HashSet<>();
+        int N = Integer.parseInt(tc[0]);
+        int M = Integer.parseInt(tc[1]);
+        Node[] graph = new Node[N];
+        for (int i = 0; i < N; i++) {
+            graph[i] = new Node();
+        }
+        for (int i = 0; i < M; i++) {
+            String[] readLine = br.readLine().split(" ");
+            int a = Integer.parseInt(readLine[0]) - 1;
+            int b = Integer.parseInt(readLine[1]) - 1;
+            graph[a].getList().add(b);
+            graph[b].getIncomingList().add(a);
+        }
+        PriorityQueue<Integer> S = new PriorityQueue<>();
+        for (int i = 0; i < N; i++) {
+            if (graph[i].getIncomingList().size() == 0) {
+                S.add(i);
+            }
+        }
+        Queue<Integer> queue = new LinkedList<>();
+        while (!S.isEmpty()) {
+            int n = S.poll();
+            if (!isDeleted.contains(n)) {
+                isDeleted.add(n);
+                queue.add(n);
+                for (int i : graph[n].getList()) {
+                    graph[i].getIncomingList().remove(n);
+                }
+                for (int i : graph[n].getList()) {
+                    if (graph[i].getIncomingList().size() == 0) {
+                        S.add(i);
+                    }
+                }
+            }
+        }
+        for (int s : queue) {
+            bw.write((s + 1) + " ");
+        }
+        bw.close();
+        br.close();
+    }
+}
+
+class Node {
+    LinkedList<Integer> list;
+    HashSet<Integer> incomingList;
+
+    public LinkedList<Integer> getList() {
+        return list;
+    }
+
+    public HashSet<Integer> getIncomingList() {
+        return incomingList;
+    }
+
+    public Node() {
+        this.list = new LinkedList<>();
+        this.incomingList = new HashSet<>();
+    }
+}
