7dcd64c686aa5164fe0b32d76289b6ad59979bd8
potionk <potionkr@gmail.com>
2021-10-18 19:00:29 +0900

    Add 23234 solution

---
+++ src/bj23234/Main.java
@@ -0,0 +1,13 @@
+package bj23234;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write("The world says hello!");
+        br.close();
+        bw.close();
+    }
+}
