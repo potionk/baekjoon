6d72662594932d1c00b46901444a3aebea3a9266
potionk <potionkr@gmail.com>
2021-10-31 19:50:46 +0900

    Add 21598 solution

---
+++ src/bj21598/Main.java
@@ -0,0 +1,16 @@
+package bj21598;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        while (n-- > 0) {
+            bw.write("SciComLove\n");
+        }
+        br.close();
+        bw.close();
+    }
+}
