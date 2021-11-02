560aa12421b56157aea0c3ab0101b0d31d6b3feb
potionk <48196324+potionk@users.noreply.github.com>
2021-11-02 23:59:03 +0900

    Add 20352 solution

---
+++ src/bj20352/Main.java
@@ -0,0 +1,13 @@
+package bj20352;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(Math.PI * 2 * Math.sqrt(Long.parseLong(br.readLine()) / Math.PI) + "");
+        br.close();
+        bw.close();
+    }
+}
