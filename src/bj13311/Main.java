58d564dd60b393f606af64e7be181ea4c6a375d2
potionk <potionkr@gmail.com>
2022-03-01 15:23:02 +0900

    add(13311): 행운의 편지
    * 수학
    * 정수론

---
+++ src/bj13311/Main.java
@@ -0,0 +1,13 @@
+package bj13311;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write("-1");
+        br.close();
+        bw.close();
+    }
+}
