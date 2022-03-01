95ff97acd82317ef2ff607fb5a0102b610d1c65a
potionk <potionkr@gmail.com>
2022-03-01 15:55:32 +0900

    add(7420): 맹독 방벽
    * 볼록 껍질

---
+++ src/bj7420/Main.java
@@ -0,0 +1,86 @@
+package bj7420;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String[] NR = br.readLine().split(" ");
+        int N = Integer.parseInt(NR[0]);
+        int L = Integer.parseInt(NR[1]);
+        LinkedList<Point> points = new LinkedList<>();
+        for (int i = 0; i < N; i++) {
+            String[] pointInfo = br.readLine().split(" ");
+            points.add(new Point(Integer.parseInt(pointInfo[1]), Integer.parseInt(pointInfo[0])));
+        }
+        points.sort(Comparator.comparing(Point::getY).thenComparing(Point::getX));
+        Point p0 = points.pollFirst();
+        points.sort((p1, p2) -> ccwCompare(ccw(p0, p2, p1)));
+        points.addFirst(p0);
+        bw.write(Math.round(calWallLength(convexHull(points), L)) + "");
+        br.close();
+        bw.close();
+    }
+
+    public static double calDist(Point a, Point b) {
+        return Math.sqrt(Math.pow((b.x - a.x), 2) + Math.pow((b.y - a.y), 2));
+    }
+
+    public static double calWallLength(Stack<Point> stack, int L) {
+        double result = calDist(stack.peek(), stack.get(0));
+        Point p = stack.pop();
+        while (!stack.isEmpty()) {
+            result += calDist(p, stack.peek());
+            p = stack.pop();
+        }
+        return result + (2 * Math.PI * L);
+    }
+
+    public static Stack<Point> convexHull(LinkedList<Point> points) {
+        Stack<Point> stack = new Stack<>();
+        stack.push(points.pollFirst());
+        stack.push(points.pollFirst());
+        while (!points.isEmpty()) {
+            Point p3 = points.pop();
+            while (stack.size() >= 2 && ccw(p3, stack.get(stack.size() - 2), stack.get(stack.size() - 1)) <= 0) {
+                stack.pop();
+            }
+            stack.push(p3);
+        }
+        return stack;
+    }
+
+    public static long ccw(Point a, Point b, Point c) {
+        return a.x * b.y + b.x * c.y + c.x * a.y - b.x * a.y - c.x * b.y - a.x * c.y;
+    }
+
+    public static int ccwCompare(long ccw) {
+        if (ccw > 0) {
+            return 1;
+        } else if (ccw == 0) {
+            return 0;
+        } else {
+            return -1;
+        }
+    }
+
+    private static class Point {
+        long x;
+        long y;
+
+        public Point(long y, long x) {
+            this.y = y;
+            this.x = x;
+        }
+
+        public long getX() {
+            return x;
+        }
+
+        public long getY() {
+            return y;
+        }
+    }
+}
