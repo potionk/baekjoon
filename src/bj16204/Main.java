1156ea0f5ac3a66f5ba9599dc85502637d73101a
potionk <potionkr@gmail.com>
2022-03-01 15:26:35 +0900

    add(16204): 카드 뽑기
    * 수학

---
+++ src/bj16204/Main.java
@@ -0,0 +1,17 @@
+package bj16204;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String[] nmk = br.readLine().split(" ");
+        int n = Integer.parseInt(nmk[0]);
+        int m = Integer.parseInt(nmk[1]);
+        int k = Integer.parseInt(nmk[2]);
+        bw.write((Math.min(m, k) + Math.min(n - m, n - k)) + "");
+        br.close();
+        bw.close();
+    }
+}
