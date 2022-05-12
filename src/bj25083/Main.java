9916da246b28ce654f8aa8624115b413618f0b32
Jongun Kim <jongun.kim@deali.net>
2022-05-12 19:40:43 +0900

    add(25083): 새싹

---
+++ src/bj25083/Main.java
@@ -0,0 +1,19 @@
+package bj25083;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        System.out.println("""
+                         ,r'"7
+                r`-_   ,'  ,/
+                 \\. ". L_r'
+                   `~\\/
+                      |
+                      |""");
+        br.close();
+        bw.close();
+    }
+}
