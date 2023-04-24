8711e691abf679ff1c8a700ba71765195dc9623d
Jongun Kim <potionkr@gmail.com>
2023-04-24 23:44:35 +0900

    add(24264): 알고리즘 수업 - 알고리즘의 수행 시간 3

---
+++ kotlin/src/main/kotlin/bj24264/Main.kt
@@ -0,0 +1,10 @@
+package bj24264
+
+import java.io.*
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val n = br.readLine().toLong()
+    print("${n * n}\n2")
+    br.close()
+}
