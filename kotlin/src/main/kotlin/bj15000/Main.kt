d5c2f93dbf0ef04ceb3e45d809ee620c45172339
Jongun Kim <potionkr@gmail.com>
2023-05-13 23:12:16 +0900

    add(15000): CAPS

---
+++ kotlin/src/main/kotlin/bj15000/Main.kt
@@ -0,0 +1,10 @@
+package bj15000
+
+import java.io.*
+import java.util.*
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    println(br.readLine().uppercase(Locale.getDefault()))
+    br.close()
+}
