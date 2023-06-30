1f7d040c91a3045b9d8990d085d3d7e48f037a4b
Jongun Kim <potionkr@gmail.com>
2023-06-30 23:51:10 +0900

    add(23530): Not A + B

---
+++ kotlin/src/main/kotlin/bj23530/Main.kt
@@ -0,0 +1,13 @@
+package bj23530
+
+import java.io.*
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val n = br.readLine().toInt()
+    for (i in 0 until n) {
+        br.readLine()
+        println("1")
+    }
+    br.close()
+}
