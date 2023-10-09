7bbea4263c7cf3ac1ed7003a555c4950c69eeea0
Jongun Kim <potionkr@gmail.com>
2023-10-09 21:14:03 +0900

    add(15551): if 3
    Aa와 BB 는 hash collision

---
+++ kotlin/src/main/kotlin/bj15551/Main.kt
@@ -0,0 +1,10 @@
+package bj15551
+
+import java.io.*
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val n = br.readLine().toInt()
+    println("Aa" + "a".repeat(n - 2))
+    println("BB" + "a".repeat(n - 2))
+}
