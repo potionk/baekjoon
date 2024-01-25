065023becb0b16443437ccc57561dbd4e9f441b8
Jongun Kim <potionkr@gmail.com>
2024-01-26 00:00:16 +0900

    add(25628): 햄버거 만들기

---
+++ kotlin/src/main/kotlin/bj25628/Main.kt
@@ -0,0 +1,9 @@
+package bj25628
+
+import java.io.*
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val (a, b) = br.readLine().split(" ").map { it.toInt() }
+    println(minOf(a / 2, b))
+}
