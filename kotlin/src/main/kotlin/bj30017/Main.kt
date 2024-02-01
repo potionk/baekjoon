95a607cae3f2f6659dbc8e628102797e6985b7c3
potionk <jongunkim.p@gmail.com>
2024-02-02 00:09:26 +0900

    add(30017): 치즈버거 만들기

---
+++ kotlin/src/main/kotlin/bj30017/Main.kt
@@ -0,0 +1,11 @@
+package bj30017
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import kotlin.math.min
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val (a, b) = br.readLine().split(" ").map { it.toInt() }
+    println(3 + 2 * min(a - 2, b - 1))
+}
