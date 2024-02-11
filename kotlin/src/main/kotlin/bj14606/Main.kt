47a596db8f3bb939f1c8348900e39327272bfd6d
Jongun Kim <potionkr@gmail.com>
2024-02-11 23:37:41 +0900

    add(14606): 피자 (Small)

---
+++ kotlin/src/main/kotlin/bj14606/Main.kt
@@ -0,0 +1,10 @@
+package bj14606
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val n = br.readLine().toInt()
+    println((n * (n - 1)) / 2)
+}
