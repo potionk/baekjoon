5af301931f3873486d23754064e04a362a8be6f9
Jongun Kim <potionkr@gmail.com>
2024-05-13 23:56:50 +0900

    add(15323): ZigZag

--- kotlin/src/main/kotlin/bj15323/Main.kt
+++ kotlin/src/main/kotlin/bj15323/Main.kt
@@ -1,10 +1,13 @@
 package bj15323
 
 import java.io.BufferedReader
+import java.io.BufferedWriter
 import java.io.InputStreamReader
+import java.io.OutputStreamWriter
 
 fun main() {
     val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
     val (k, n) = br.readLine().split(" ").map { it.toInt() }
     val map = mutableMapOf<Char, MutableList<String>>()
     val count = mutableMapOf<Char, Int>()
@@ -18,7 +21,8 @@ fun main() {
     repeat(n) {
         val c = br.readLine()[0]
         val idx = count.getOrDefault(c, 0) % map[c]!!.size
-        println(map[c]!![idx])
+        bw.write("${map[c]!![idx]}\n")
         count[c] = idx + 1
     }
+    bw.close()
 }
