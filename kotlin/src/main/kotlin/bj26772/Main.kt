f3a31cbbb8b13468868833dd5d77e1002290863b
Jongun Kim <potionkr@gmail.com>
2024-08-03 23:58:18 +0900

    add(26772): Poziome serca

---
+++ kotlin/src/main/kotlin/bj26772/Main.kt
@@ -0,0 +1,18 @@
+package bj26772
+
+import java.io.*
+import kotlin.math.max
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val n = br.readLine().toInt()
+    println(" @@@   @@@  ".repeat(n))
+    println("@   @ @   @ ".repeat(n))
+    println("@    @    @ ".repeat(n))
+    println("@         @ ".repeat(n))
+    println(" @       @  ".repeat(n))
+    println("  @     @   ".repeat(n))
+    println("   @   @    ".repeat(n))
+    println("    @ @     ".repeat(n))
+    println("     @      ".repeat(n))
+}
