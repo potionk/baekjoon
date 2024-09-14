d2824f275b25be9263ba3013d81c8a20ffb362bc
Jongun Kim <potionkr@gmail.com>
2024-09-14 23:59:54 +0900

    add(20336): Haughty Cuisine

---
+++ kotlin/src/main/kotlin/bj20336/Main.kt
@@ -0,0 +1,12 @@
+package bj20336
+
+import java.io.*
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val n = br.readLine().toInt()
+    repeat(n - 1) {
+        br.readLine()
+    }
+    println(br.readLine().split(" ").drop(1).joinToString("\n"))
+}
