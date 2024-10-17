fa897a31798f99b7e3f9b8a14731425a3d0490d8
Jongun Kim <potionkr@gmail.com>
2024-10-17 23:48:52 +0900

    add(32288): 바코드 닉네임

---
+++ kotlin/src/main/kotlin/bj32288/Main.kt
@@ -0,0 +1,9 @@
+package bj32288
+
+import java.io.*
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    br.readLine()
+    println(br.readLine().replace("I", "i").replace("l", "L"))
+}
