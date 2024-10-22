1796478cc5379ff4f846a290b9fe3b8c371159f3
Jongun Kim <potionkr@gmail.com>
2024-10-22 23:56:43 +0900

    add(32384): 사랑은 고려대입니다

---
+++ kotlin/src/main/kotlin/bj32384/Main.kt
@@ -0,0 +1,8 @@
+package bj32384
+
+import java.io.*
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    println("LoveisKoreaUniversity ".repeat(br.readLine().toInt()))
+}
