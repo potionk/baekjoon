591ebd823ad41d752f0da08c5acd2c90aa3ef2ee
Jongun Kim <potionkr@gmail.com>
2024-11-15 23:56:30 +0900

    add(32652): 아카라카 2

---
+++ kotlin/src/main/kotlin/bj32652/Main.kt
@@ -0,0 +1,10 @@
+package bj32652
+
+import java.io.*
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val n = br.readLine().toInt()
+    print("AKARAKA")
+    println("RAKA".repeat(n - 1))
+}
