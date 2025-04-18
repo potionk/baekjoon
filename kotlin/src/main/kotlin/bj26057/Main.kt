87bba02ea4c6e8d90ab696afc1c17cd3736c63e1
Jongun Kim <jongunkim.p@gmail.com>
2025-04-18 22:59:27 +0900

    add(26057): Большой удой

---
+++ kotlin/src/main/kotlin/bj26057/Main.kt
@@ -0,0 +1,8 @@
+package bj26057
+
+fun main() {
+    val reader = System.`in`.bufferedReader()
+    val l = reader.readLine().toInt()
+    val t = reader.readLine().toInt()
+    println(2 * t - l)
+}
